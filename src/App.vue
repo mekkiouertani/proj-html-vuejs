24d1ac6f3b8c2fa6ef5c58453c21f1dcd1bfe856
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-11-21 11:16:50 +0100

    add structure navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>hello</div>
+  <NavComponent />
+  <HeroComponent />
+  <MainComponent />
+  <FooterComponent />
 </template>
 
 <script>
+import FooterComponent from "./components/FooterComponent.vue";
+import HeroComponent from "./components/HeroComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
+import NavComponent from "./components/NavComponent.vue";
+
 export default {
   name: "App",
+  components: { NavComponent, HeroComponent, MainComponent, FooterComponent },
 };
 </script>
 
---
+++ src/components/NavComponent.vue
@@ -0,0 +1,78 @@
+<template>
+  <nav class="navbar w-100 navbar-expand-md">
+    <div class="container d-flex justify-content-between">
+      <div class="nav-logo">
+        <img src="../images/logo-autocar11.png" alt="" />
+      </div>
+      <div class="right-nav">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown link
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  height: 100px;
+  position: fixed;
+  z-index: 9999;
+  background-color: red;
+}
+
+.nav-logo {
+  width: 200px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
